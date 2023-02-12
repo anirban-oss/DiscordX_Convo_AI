112f5b371535dbcb61f097f1ecceb5502a71b07e
Anirban Chatterjee <anirbanc88@gmail.com>
2023-02-12 23:32:40 +0530

    Update A basic bot.py

--- A basic bot.py
+++ A basic bot.py
@@ -40,3 +40,26 @@ client.run("<discord_bot_token>")
 
 # The bot connects to Discord using the provided bot token. 
 # Replace <discord_bot_token> with your own Discord bot token.
+
+# Research exmples
+
+# # import required modules
+# from bs4 import BeautifulSoup
+# import requests
+
+# # get URL
+# page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# # scrape webpage
+# soup = BeautifulSoup(page.content, 'html.parser')
+
+# # create object
+# object = soup.find(id="mp-left")
+
+# # find tags
+# items = object.find_all(class_="mp-h2")
+# result = items[0]
+
+# # display tags
+# print(result.prettify())
+
